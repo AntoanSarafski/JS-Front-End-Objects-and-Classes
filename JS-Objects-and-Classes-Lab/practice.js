11da4bf9aae155362b8ea807d5ddf2f30fddbbc6
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-07-09 12:19:50 +0300

    Updated practice with for loop for obj

--- JS-Objects-and-Classes-Lab/practice.js
+++ JS-Objects-and-Classes-Lab/practice.js
@@ -1,13 +1,29 @@
-const person = {
-  firstName: "Tony",
-  lastName: "CSKA",
-  age: 31,
+// const person = {
+//   firstName: "Tony",
+//   lastName: "CSKA",
+//   age: 31,
 
-  printNames: function () {
-    console.log(
-      `First Name: ${person.firstName}\nLast Name: ${person.lastName}\nAge: ${person.age}`
-    );
-  },
+//   printNames: function () {
+//     console.log(
+//       `First Name: ${this.firstName}\nLast Name: ${this.lastName}\nAge: ${this.age}`
+//     );
+//   },
+// };
+
+// person.printNames();
+
+function printCityInfo(city) {
+  for (const key in city) {
+    console.log(key + " -> " + city[key]);
+  }
+}
+
+const city = {
+  name: "Sofia",
+  area: 482,
+  population: 1234567,
+  postCode: 1000,
+  country: "Bulgaria",
 };
 
-person.printNames();
+printCityInfo(city);
