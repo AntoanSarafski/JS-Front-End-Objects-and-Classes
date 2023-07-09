116db3bff3cb2417ae087849fa29fbaf6ab23008
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-07-09 23:20:58 +0300

    Update PhoneBook with FOR OF and FOR IN
    Same solution.

--- JS-Objects-and-Classes-Lab/Phone-Book/phone-book.js
+++ JS-Objects-and-Classes-Lab/Phone-Book/phone-book.js
@@ -1,18 +1,15 @@
-function collectPhones(arr) {
-  const phoneObj = arr.reduce((acc, curr) => {
-    const [name, phone] = curr.split(" ");
-    acc[name] = phone;
-    return acc;
-  }, {});
+function solve(input) {
+  let phoneBook = {};
+  for (let line of input) {
+    let tokens = line.split(" ");
+    let name = tokens[0];
+    let phone = tokens[1];
+    phoneBook[name] = phone;
+  }
 
-  Object.entries(phoneObj).forEach(([key, value]) => {
-    console.log(`${key} -> ${value}`);
-  });
+  for (let key in phoneBook) {
+    console.log(`${key} -> ${phoneBook[key]}`);
+  }
 }
 
-collectPhones([
-  "Tim 084984563",
-  "Peter 084598158",
-  "Bill 089846851",
-  "Tim 0877989599",
-]);
+solve(["Tim 084984563", "Peter 084598158", "Bill 089846851", "Tim 0877989599"]);
