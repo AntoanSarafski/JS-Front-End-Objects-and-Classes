712d1ff9245a8ed0518b82fbcb10d68ba5003cd6
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-07-09 21:14:21 +0300

    Updated with 01.City Taxes
    Commit with solution include obj, methods and .this usage case.

---
+++ JS-Objects-and-Classes-Lab/01. City Taxes/city-taxes.js
@@ -0,0 +1,25 @@
+function createCity(name, population, treasury) {
+  return {
+    name,
+    population,
+    treasury,
+    taxRate: 10,
+    collectTaxes() {
+      this.treasury += this.population * this.taxRate;
+    },
+
+    applyGrowth(percentage) {
+      this.population += this.population * percentage;
+    },
+
+    applyRecession(percentage) {
+      this.population -= this.population * percentage;
+    },
+  };
+}
+
+const city = createCity("Tortuga", 7000, 15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(0.05);
+console.log(city.population);
