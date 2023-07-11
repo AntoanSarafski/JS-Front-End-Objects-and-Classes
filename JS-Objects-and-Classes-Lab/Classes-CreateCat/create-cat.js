78e732aa5e750e71911f67cff4acfeaae8e8a875
Antoan Sarafski <98612405+AntoanSarafski@users.noreply.github.com>
2023-07-11 09:38:47 +0300

    Update Create-cat with class and method.

--- JS-Objects-and-Classes-Lab/Classes-CreateCat/create-cat.js
+++ JS-Objects-and-Classes-Lab/Classes-CreateCat/create-cat.js
@@ -1,18 +1,23 @@
 class Cat {
-constructor(name, age) {
-    this.name = name;
-    this.age = age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    meow() {
+        console.log(`Name: ${this.name}, Age: ${this.age} says meow.`);
+    }
 }
-}
+
 
 function createCats(input) {
     input.forEach((line) => {
         const [name, age] = line.split(" ");
-        console.log(name, age);
+        console.log(`Name: ${name}, Age: ${age}`);
         const cat = new Cat(name, age);
+        console.log(cat)
+        cat.meow();
     });
 }
-
-
 
 createCats(["Mellow 2" , "Tom 5"]);
